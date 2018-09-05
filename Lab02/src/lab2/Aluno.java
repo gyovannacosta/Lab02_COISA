c5cb048a71c501d8bd8c9b40e7ddfb484203eb0e
Gyovanna Costa <deborah.gyovanna.costa@ccc.ufcg.edu.br>
2018-09-05 10:44:26 -0300

    Criando classe Aluno

---
+++ Lab02/src/lab2/Aluno.java
@@ -0,0 +1,85 @@
+package lab2;
+
+public class Aluno {
+	private String nome;
+	
+	public Aluno(String nome) {
+		this.nome = nome;
+	}
+	
+	public void cadastraLaboratorio(String nomeLaboratorio) {
+
+	}
+	
+	public void cadastraLaboratorio(String nomeLaboratorio, int cota) {
+		
+	}
+	
+	public void consomeEspaco(String nomeLaboratorio, int mbytes) {
+		
+	}
+	
+	public void liberaEspaco(String nomeLaboratorio, int mbytes) {
+		
+	}
+	
+	public boolean atingiuCota(String nomeLaboratorio) {
+		return false;
+	}
+	
+	public String laboratorioToString(String nomeLaboratorio) {
+		return null;
+	}
+	
+	public void cadastraDisciplina(String nomeDisciplina) {
+		
+	}
+	
+	public void cadastraHoras(String nomeDisciplina, int horas) {
+		
+	}
+	
+	public void cadastraNota(String nomeDisciplina, int nota, double valorNota) {
+		
+	}
+	
+	public boolean aprovado(String nomeDisciplina) {
+		return false;
+	}
+	
+	public String disciplinaToString(String nomeDisciplina) {
+		return null;
+	}
+	
+	public void cadastraCantina(String nomeCantina) {
+		
+	}
+	
+	public void cadastraLanche(String nomeCantina, int qtdItens, int valorCentavos) {
+		
+	}
+	
+	public void pagarConta(String nomeCantina, int valorCentavos) {
+		
+	}
+	
+	public int getFaltaPagar(String nomeCantina) {
+		return 0;
+	}
+	
+	public String cantinaToString(String nomeCantina) {
+		return null;
+	}
+	
+	public void defineSaudeMental(String valor) {
+		
+	}
+	
+	public void defineSaudeFisica(String valor) {
+		
+	}
+	
+	public String getStatusGeral() {
+		return null;
+	}
+}
